e9c14e508d38e87dc6377c8a68b3a517687c9d5d
Pineappleman123 <86864397+Pineappleman123@users.noreply.github.com>
2022-12-19 14:26:51 -0800

    prevents error with incorrect stock symbol

--- main.py
+++ main.py
@@ -11,10 +11,15 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 results = soup.find(id="maincontent")
 
-current_price = results.find("h2", class_="intraday__price").find("bg-quote").text
+try:
+    current_price = results.find("h2", class_="intraday__price").find("bg-quote").text
 
-info = results.find("div", class_="group group--elements left").find_all("li", class_="kv__item")
-for info in info:
-    print(info.text)
+    info = results.find("div", class_="group group--elements left").find_all("li", class_="kv__item")
+    print("Useful info: \n")
+    for info in info:
+        print(info.text.strip())
+        print()
 
-print("Current price: $" + current_price)
+    print("Current price: $" + current_price)
+except:
+    print("Incorrect stock symbol")
